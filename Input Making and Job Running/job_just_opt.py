b52bd25296426b7cecc3b7f5c9527fa6a82668fb
Ezra Alexander <ezraa@mit.edu>
2024-01-15 17:04:25 -0500

    Updated script for quickly running qchem optimizations from .xyz files

--- Input Making and Job Running/job_just_opt.py
+++ Input Making and Job Running/job_just_opt.py
@@ -2,23 +2,23 @@ import numpy as np
 import sys
 from qchem_helper import write_input, write_geom, my_get_rem_sp, read_xyz
 
-#the .xyz file, without the ".xyz"
+#the .xyz file. Note that the file name is assumed to be unopt_DESCRIPTOR.xyz
 xyz=sys.argv[1]
 #template qchem opt .in
 opt=sys.argv[2]
 #charge
 charge=sys.argv[3]
 
+job_name=xyz[6:-4]
 
-coords, atoms = read_xyz(xyz+".xyz")
-
+coords, atoms = read_xyz(xyz)
 
 #the opt
 opt_rem, trash = my_get_rem_sp(opt)
 
 opt_rem =''.join(opt_rem)
 
-write_input("opt_"+xyz+".in", atoms, coords, opt_rem, charge + " 1 \n")
+write_input("opt_"+job_name+".in", atoms, coords, opt_rem, charge + " 1 \n")
 
 
 
--- Input Making and Job Running/job_just_opt.sh
+++ Input Making and Job Running/job_just_opt.sh
@@ -2,14 +2,26 @@
 
 #This script is meant to run an optimization job and a sinle-point unoptimized job given an .xyz file
 
-#the .xyz file, without the ".xyz"
+#the .xyz file. Note that the file name is assumed to be unopt_DESCRIPTOR.xyz
 xyz=$1
 #template qchem opt .in
 opt=$2
 #charge
 charge=$3
+#number of corees
+cores=$4
+#priority. If on Ulysses, leave blank
+priority=$5
 
-#this version is written for Ulysses but only this line needs to be changed for telemachus (path to code)
-python3 /work/ezraa/code/job_just_opt.py $xyz $opt $charge
+job_name=${xyz%.*}
+job_name=${job_name#*_}
 
-sqthis -J "opt_${xyz}" -c 8 -t unlimited qchem.latest -nt 8 "opt_${xyz}.in" "opt_${xyz}.out"
+python3 ~/code/job_just_opt.py $xyz $opt $charge
+
+#note that on Ulysses the bash version requires if [ ]; instead
+if [[ -z $priority ]]; then
+	sqthis -J "opt_${job_name}" -c $cores -t unlimited qchem.latest -nt $cores "opt_${job_name}.in" "opt_${job_name}.out"
+else
+	sqthis -J "opt_${job_name}" -c $cores -t unlimited -p $priority qchem.latest -nt $cores "opt_${job_name}.in" "opt_${job_name}.out"
+fi
+
