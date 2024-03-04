692f1710bef8a71c4cf104b6c2cc14d33c323a01
Ezra Alexander <ezraa@mit.edu>
2024-03-04 15:32:56 -0500

    Now supports plotting histograms

--- Data Analysis/catalog_dot_parameter.py
+++ Data Analysis/catalog_dot_parameter.py
@@ -14,10 +14,13 @@ from scipy import stats
 #this is probably more of a hard-coding sort of situation but I can pretend
 
 file=sys.argv[1] #ideally the defect catalog itself, in my dropbox
-measure=sys.argv[2] #do you want to average a metric ("av") or sum ("sum")
+measure=sys.argv[2] #do you want to average a metric ("av"), sum ("sum"), or plot a histogram ("hist")
 column=sys.argv[3] #the column index you want to target
-constraint_column=sys.argv[4] #the column of the constraint you want to target
-constraint=sys.argv[5] #the value the constraint should have
+constrained=False
+if len(sys.argv)>4:
+	constraint_column=sys.argv[4] #the column of the constraint you want to target. if blank, all dots for which column is defined
+	constraint=sys.argv[5] #the value the constraint should have. if blank, all dots for which column is defined
+	constrained=True
 
 
 #read in the defect catalog
@@ -32,23 +35,34 @@ for i in range(2000):
 	if ws["A"+str(i+1)].value: #how we seperate different dots
 
 		if ws["A"+str(i+1)].value!="Small":
-			if ws["I"+str(i+1)].value=="Defect": #some manual additional constraints, because that's easier
-				if ws["M"+str(i+1)].value>0:
+			#if ws["I"+str(i+1)].value=="Defect": #some manual additional constraints, because that's easier
+				#if ws["M"+str(i+1)].value>0:
 
 					dot_row=dot_row+1
 					if dot_row==1: #once per dot
 
 						if ws[column+str(i+1)].value and (not isinstance(ws[column+str(i+1)].value,str)): #if parameter is defined and is a number
 
-							if ws[constraint_column+str(i+1)].value==constraint: #if constraint
-									dataset.append(ws[column+str(i+1)].value)
+							if constrained:
+								if ws[constraint_column+str(i+1)].value==constraint: #if constraint
+										dataset.append(ws[column+str(i+1)].value)
+							else:
+								dataset.append(ws[column+str(i+1)].value)
+
 	else:
 		dot_row=0
 
 dataset=np.array(dataset)
 if measure=="av" or measure=="average":
-	print("The average value of",ws[column+str(1)].value, "for dots where",ws[constraint_column+str(1)].value, "is",constraint, "is",np.average(dataset))
+	print("The average value of",ws[column+str(1)].value, "is",np.average(dataset))
 elif measure=="sum":
-	print("The sum of",ws[column+str(1)].value, "for dots where",ws[constraint_column+str(1)].value, "is",constraint, "is",np.sum(dataset))
+	print("The sum of",ws[column+str(1)].value, "is",np.sum(dataset))
+elif measure=="hist":
+	plt.hist(dataset,bins=[-.4,-.35,-0.3,-0.25,-0.2,-.15,-.1,-.05,0,0.05,.1,.15,.2,.25,.3,.35,.4])
+	plt.title(ws[column+"1"].value)
+	minor_locator = MultipleLocator(base=0.05)
+	plt.gca().xaxis.set_minor_locator(minor_locator)
+	#plt.hist(dataset)
+	plt.show()
 else:
-	print("Second argument should be the measurement type: 'av' for average and 'sum' for sum")
+	print("Second argument should be the measurement type: 'av' for average, 'sum' for sum, 'hist' for a histogram")
